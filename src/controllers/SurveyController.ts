cab5de1618f1c8e14d5ccdd447d808fee228a2e9
LucianoPierdona <lucianompj1@gmail.com>
2021-02-24 11:16:01 -0300

    finished create survey

--- src/controllers/SurveyController.ts
+++ src/controllers/SurveyController.ts
@@ -1,7 +1,22 @@
 import { Request, Response } from 'express';
+import { getCustomRepository } from 'typeorm';
+import { SurveysRepository } from '../repositories/surveysRepository';
 
 class SurveyController {
-  async create(req: Request, res: Response) {}
+  async create(req: Request, res: Response) {
+    const { title, description } = req.body;
+
+    const surveyRepository = getCustomRepository(SurveysRepository);
+
+    const survey = surveyRepository.create({
+      description,
+      title,
+    });
+
+    await surveyRepository.save(survey);
+
+    return res.json(survey);
+  }
 }
 
 export { SurveyController };
