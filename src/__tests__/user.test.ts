72c6c49d01e5e012fa034e08698bf07c287da70d
LucianoPierdona <lucianompj1@gmail.com>
2021-02-24 11:52:30 -0300

    fixed tests

--- src/__tests__/user.test.ts
+++ src/__tests__/user.test.ts
@@ -1,13 +1,19 @@
 import request from 'supertest';
+import { Connection } from 'typeorm';
 import { app } from '../app';
 
 import createConnection from '../database';
 
 describe('Users', () => {
-  beforeAll(async () => {
-    const connection = await createConnection();
+  let connection: Connection;
 
+  beforeAll(async () => {
+    connection = await createConnection();
     await connection.runMigrations();
+  });
+
+  afterAll(async () => {
+    await connection.dropDatabase();
   });
 
   it('create a new user', async () => {
@@ -17,5 +23,14 @@ describe('Users', () => {
     });
 
     expect(response.status).toEqual(201);
+  });
+
+  it('throws an error if the user already exist', async () => {
+    const response = await request(app).post('/users').send({
+      email: 'luciano@gmail.com',
+      name: 'Luciano',
+    });
+
+    expect(response.status).toEqual(400);
   });
 });
