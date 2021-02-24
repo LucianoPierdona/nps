697c25fe290c7e028881899b80c8f346ba09ed7c
LucianoPierdona <lucianompj1@gmail.com>
2021-02-24 11:27:09 -0300

    created first test

---
+++ src/__tests__/first.test.ts
@@ -0,0 +1,9 @@
+describe('First', () => {
+  it('must be possible sum 2 numbers', () => {
+    expect(1 + 1).toEqual(2);
+  });
+
+  it('must be possible sum 2 numbers', () => {
+    expect(2 + 2).not.toEqual(2);
+  });
+});
