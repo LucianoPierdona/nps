e3c2f40923a87b0da15260846a08c3f64405cc2c
LucianoPierdona <lucianompj1@gmail.com>
2021-02-25 09:48:47 -0300

    created SurveyUser model

---
+++ src/models/SurveyUser.ts
@@ -0,0 +1,33 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+import { v4 as uuid } from 'uuid';
+
+@Entity('surveys')
+class SurveyUser {
+  @PrimaryGeneratedColumn('uuid')
+  readonly id: string;
+
+  @Column({ type: 'uuid' })
+  userId: string;
+
+  @Column({ type: 'uuid' })
+  surveyId: string;
+
+  @Column({ type: 'text' })
+  value: string;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  constructor() {
+    if (!this.id) {
+      this.id = uuid();
+    }
+  }
+}
+
+export { SurveyUser };
